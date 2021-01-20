db204b9c655f16d40f2d09215620c694816479f6
mizukami1234 <mizukami19960914@gmail.com>
2021-01-20 15:14:24 +0900

    商品出品時のバリデーション設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ApplicationRecord
 
   belongs_to :user
+
+
+  validates :product_name, presence: true
+  validates :text, presence: true
+  validates :price, presence: true
+  validates :category_id, presence: true
+  validates :product_condition_id, presence: true
+  validates :shipping_charge_id, presende: true
+  validates :shipment_source_id, presence: true
+  validates :estimated_shipping_date_id, presence: true
+
 end
