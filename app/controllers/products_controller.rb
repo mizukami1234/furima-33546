dd73c781967f24a8ae38d02bfb7dcfc33b02e66d
mizukami1234 <mizukami19960914@gmail.com>
2021-01-20 22:14:43 +0900

    出品機能とバリデーション

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,36 @@
 class ProductsController < ApplicationController
   
   def index 
+    @product = Product.all
   end
   
   def new
+    @product = Product.new
   end
 
-  def create(product_params)
+  # render 
+
+  # new -> create -> new(view)
+
+  # rediredct_to
+
+  # new -> create -> route.rb(new) -> new
+
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
 
 
   private
 
   def product_params
-    params.require(:product).permit(:product_name, :text, :price, :image).merge(user_id: current_user.id)
+    params.require(:product).permit(:product_name, :text, :price, :image, :category_id, :product_condition_id, :shipping_charge_id, :shipment_source_id, :estimated_shipping_date_id).merge(user_id: current_user.id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_condition
+  belongs_to :shipping_charge
+  belongs_to :shipment_source
+  belongs_to :estimated_shipping_date
 
   belongs_to :user
   has_one_attached :image
@@ -6,13 +12,15 @@ class Product < ApplicationRecord
 
   validates :product_name, presence: true
   validates :text, presence: true
-  validates :price, presence: true
-  validates :category_id, presence: true
-  validates :product_condition_id, presence: true
-  validates :shipping_charge_id, presende: true
-  validates :shipment_source_id, presence: true
-  validates :estimated_shipping_date_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 } 
+  validates :product_condition_id, presence: true, numericality: { other_than: 1 } 
+  validates :shipping_charge_id, presence: true, numericality: { other_than: 1 }
+  validates :shipment_source_id, presence: true, numericality: { other_than: 0 }
+  validates :estimated_shipping_date_id, presence: true, numericality: { other_than: 1 }
 
+  with_options presence: true, numericality: { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999}, format: { with: /\A[0-9]+\z/ , message: '半角数字のみ' } do
+    validates :price
+  end
 
 
 end
