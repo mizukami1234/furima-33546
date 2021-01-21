9438f476189794ed31f4a0a4bdb164f4ac35b25e
mizukami1234 <mizukami19960914@gmail.com>
2021-01-21 18:25:57 +0900

    商品出品機能の単体テスト

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,20 +7,28 @@ class Product < ApplicationRecord
   belongs_to :estimated_shipping_date
 
   belongs_to :user
+
   has_one_attached :image
+  validates :image, presence: true
 
 
   validates :product_name, presence: true
   validates :text, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1 } 
-  validates :product_condition_id, presence: true, numericality: { other_than: 1 } 
-  validates :shipping_charge_id, presence: true, numericality: { other_than: 1 }
-  validates :shipment_source_id, presence: true, numericality: { other_than: 0 }
-  validates :estimated_shipping_date_id, presence: true, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :product_condition_id, numericality: { other_than: 1 } 
+  validates :shipping_charge_id, numericality: { other_than: 1 }
+  validates :shipment_source_id, numericality: { other_than: 0 }
+  validates :estimated_shipping_date_id, numericality: { other_than: 1 }
 
-  with_options presence: true, numericality: { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999}, format: { with: /\A[0-9]+\z/ , message: '半角数字のみ' } do
-    validates :price
+
+  validates :price, presence: true
+
+  with_options numericality: { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999}, format: { with: /\A[0-9]+\z/ , message: '半角数字のみ' } do
+    validates :price 
   end
+
+
+ 
 
 
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :product do
-    
+    product_name               { "商品名だよ" }
+    text                       { "3年前に購入したものの使わずに置いてありました" }
+    price                      { 5000 }
+    # user_id                  { 2 }
+    category_id                { 2 }
+    product_condition_id       { 2 }
+    shipping_charge_id         { 2 }
+    shipment_source_id         { 2 }
+    estimated_shipping_date_id { 2 }
+
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
+    association :user
+
   end
 end
