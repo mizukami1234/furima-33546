47ca4966186ee338a73308694821ab1b4f00f9b2
mizukami1234 <mizukami19960914@gmail.com>
2021-01-25 21:01:09 +0900

    商品名・価格・配送料の表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,8 @@
 class ProductsController < ApplicationController
   
-  #def index 
-   # @product = Product.all
-  #end
+  def index 
+    @products = Product.all
+  end
   
   def new
     @product = Product.new
