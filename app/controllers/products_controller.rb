7fb62d85d82bd21eecd73666de27bbeba236199a
mizukami1234 <mizukami19960914@gmail.com>
2021-01-26 20:30:17 +0900

    edit/updateアクションの定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,6 +23,19 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update 
+    product = Product.find(params[:id])
+    if product.update(product_params)
+      redirect_to product_path(product.id)
+    else
+      render 'edit'
+    end
+  end
+
 
   private
 
