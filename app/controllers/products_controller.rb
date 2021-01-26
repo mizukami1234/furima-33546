ff96b0d7063a8787f4c015a78fb6b4ca00faeadb
mizukami1234 <mizukami19960914@gmail.com>
2021-01-26 20:56:25 +0900

    コントローラーの編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   
   def index 
     @products = Product.includes(:user).order("created_at DESC")
@@ -37,9 +38,17 @@ class ProductsController < ApplicationController
   end
 
 
-  private
+private
 
-  def product_params
+def product_params
     params.require(:product).permit(:product_name, :text, :price, :image, :category_id, :product_condition_id, :shipping_charge_id, :shipment_source_id, :estimated_shipping_date_id).merge(user_id: current_user.id)
+end
+
+def move_to_index
+  product = Product.find(params[:id])
+  unless current_user.id == product.user.id
+    redirect_to action: :index
   end
+end
+
 end
