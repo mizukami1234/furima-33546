e9e005e823e6195887539d8fed545b49c12537b4
mizukami1234 <mizukami19960914@gmail.com>
2021-02-02 13:02:01 +0900

    ストロングパラメーター修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,9 +10,10 @@ end
 
 
 def create
-  binding.pry
+
   @product = Product.find(params[:product_id])
   @order_address = OrderAddress.new(order_params)
+
 
     if @order_address.valid?
       pay_product
@@ -28,12 +29,13 @@ end
 private
 
 def order_params
-  params.permit(:postal_code, :shipment_source_id, :city, :house_number, :building_name, :phone_number, :prefecture_id).merge(user: current_user.id, product: params[:product_id], token: params[:token])
+  params.require(:order_address).permit(:postal_code, :shipment_source_id, :city, :house_number, :building_name, :phone_number, :prefecture_id).merge(user: current_user.id, product: params[:product_id], token: params[:token])
 end
 
 def pay_product
-  Payjp.api_key = "sk_test_230ca29363dd9a8a84758209"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
     Payjp::Charge.create(
+      amount: @product.price,
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
